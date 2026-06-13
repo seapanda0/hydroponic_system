1eea1f7320cd72e86e219c5b5fb543c1ea9e175d
seapanda0 <1211112330@student.mmu.edu.my>
2026-06-13 13:01:52 +0800

    Stop tracking wifi.h

--- main/wifi.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_H
-#define WIFI_H
-
-// Change these to match your WiFi access point credentials
-#define WIFI_SSID "your_wifi_ssid"
-#define WIFI_PASS "your_wifi_password"
-
-#endif // WIFI_H
